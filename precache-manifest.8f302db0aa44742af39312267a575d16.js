c6ca69aca6ab6b0075da5d718913f6088fa35afa
DumanTorbayev <dumanchikt@gmail.com>
2020-08-22 23:45:21 +0600

    Updates

---
+++ precache-manifest.8f302db0aa44742af39312267a575d16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9c2b7e51a71bb6134f2ac7546a8b3a",
+    "url": "/react-test-app/index.html"
+  },
+  {
+    "revision": "ab3168ffcef12810cc0f",
+    "url": "/react-test-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b8abbcacf30f6ec030af",
+    "url": "/react-test-app/static/css/main.eecb8b36.chunk.css"
+  },
+  {
+    "revision": "ab3168ffcef12810cc0f",
+    "url": "/react-test-app/static/js/2.286aa19b.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/react-test-app/static/js/2.286aa19b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8abbcacf30f6ec030af",
+    "url": "/react-test-app/static/js/main.47d2272e.chunk.js"
+  },
+  {
+    "revision": "c88b0ed9d5d3e0f274ce",
+    "url": "/react-test-app/static/js/runtime-main.29b5fb01.js"
+  },
+  {
+    "revision": "87e9062e6fa212fd144815ca88f122a3",
+    "url": "/react-test-app/static/media/open-menu.87e9062e.svg"
+  }
+]);
